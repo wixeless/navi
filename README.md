33e671c754015fc5b0ca88cb46e32fa0c58bbcf7
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-06-14 23:15:19 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ethermine Pool Monitor
+
+![mvvm](https://user-images.githubusercontent.com/5779415/59525975-99cf2080-8ef9-11e9-8aeb-329caad89dcd.png)
---
+++ app/src/main/java/com/marvel/stark/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.marvel.stark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * Created by Jahongir on 6/14/2019.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
