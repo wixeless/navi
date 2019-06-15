f57be0bb219bab9abea6575281b73cf4e64bdb6d
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-06-15 15:58:15 +0600

    Navigation UI component setup

---
+++ app/src/main/java/com/marvel/stark/ui/WalletSettingsFragment.kt
@@ -0,0 +1,18 @@
+package com.marvel.stark.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marvel.stark.R
+import com.marvel.stark.di.factory.Injectable
+
+/**Created by Jahongir on 6/15/2019.*/
+
+class WalletSettingsFragment : Fragment(), Injectable {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_wallet_settings, container, false)
+    }
+}
