222e20155f09cf8406f42d4eea43c71dc6507d2a
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-06-19 21:09:22 +0600

    Navigation UI bind to MainActivity

--- app/src/main/java/com/marvel/stark/MainActivity.kt
+++ app/src/main/java/com/marvel/stark/MainActivity.kt
@@ -1,14 +1,45 @@
 package com.marvel.stark
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.idescout.sql.SqlScoutServer
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
+
 /**
  * Created by Jahongir on 6/14/2019.
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope, HasSupportFragmentInjector {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SqlScoutServer.create(this, packageName)
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        val hostFragment: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.host_fragment) as NavHostFragment? ?: return
+        val navController = hostFragment.navController
+        appBarConfiguration =AppBarConfiguration(navController.graph)
+
+    }
+
+
+    override fun supportFragmentInjector() = dispatchingAndroidInjector
 }
