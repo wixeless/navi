5cb2945ea1865fbd53adaefd3df91c430e14f65a
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-06-21 14:33:09 +0600

    Bind workers with wallet

--- app/src/main/java/com/marvel/stark/models/Dto.kt
+++ app/src/main/java/com/marvel/stark/models/Dto.kt
@@ -7,7 +7,14 @@ import com.squareup.moshi.Json
 /**Created by Jahongir on 6/19/2019.*/
 
 data class DashboardDto(@field:Json(name = "currentStatistics") val wallet: Wallet,
-                        @field:Json(name = "workers") val workers: List<Worker>)
+                        @field:Json(name = "workers") val workers: List<Worker>) {
+    fun setWallet(wallet: Wallet) {
+        this.wallet.id = wallet.id
+        this.wallet.name = wallet.name
+        this.wallet.coin = wallet.coin
+        this.wallet.address = wallet.address
+    }
+}
 
 
 data class AddWalletEntity(val address: String, val name: String)
--- app/src/main/java/com/marvel/stark/room/Models.kt
+++ app/src/main/java/com/marvel/stark/room/Models.kt
@@ -1,14 +1,12 @@
 package com.marvel.stark.room
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 import com.marvel.stark.models.Coin
 import com.squareup.moshi.Json
 
 /**Created by Jahongir on 6/15/2019.*/
 
-@Entity
+@Entity(tableName = "wallet")
 data class Wallet(@PrimaryKey(autoGenerate = true) var id: Long = 0,
                   var address: String = "",
                   var coin: Coin = Coin.ETH,
@@ -17,9 +15,18 @@ data class Wallet(@PrimaryKey(autoGenerate = true) var id: Long = 0,
                   @ColumnInfo(name = "current_hashrate") var currentHashrate: Double = 0.0,
                   @ColumnInfo(name = "reported_hashrate") var reportedHashrate: Double = 0.0,
                   @ColumnInfo(name = "active_workers") var activeWorkers: Int = 0,
-                  @ColumnInfo(name = "last_seen") val lastSeen: Long)
+                  @ColumnInfo(name = "last_seen") var lastSeen: Long = 0L)
 
-@Entity
+@Entity(
+        tableName = "worker",
+        indices = [Index(value = ["wallet_id"])],
+        foreignKeys = [ForeignKey(
+                entity = Wallet::class,
+                parentColumns = arrayOf("id"),
+                childColumns = arrayOf("wallet_id"),
+                onDelete = ForeignKey.CASCADE,
+                onUpdate = ForeignKey.CASCADE)]
+)
 data class Worker(@PrimaryKey(autoGenerate = true) val id: Long,
                   @ColumnInfo(name = "wallet_id") var walletId: Long,
                   @field:Json(name = "worker") val name: String,
