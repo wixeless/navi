fac3ecfb3cd0d44abafbb739f8933dc33e2f50c9
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-06-24 15:23:53 +0600

    Retrofit Call wrapper into coroutine

---
+++ app/src/main/java/com/marvel/stark/rest/RetrofitExtension.kt
@@ -0,0 +1,51 @@
+package com.marvel.stark.rest
+
+import com.marvel.stark.rest.livedata.ApiResponse
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.suspendCancellableCoroutine
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
+import kotlin.coroutines.resume
+
+/**Created by Jahongir on 6/21/2019.*/
+
+suspend fun <T : Any> Call<T>.awaitResult(): ApiResponse<T> {
+    return suspendCancellableCoroutine { continuation ->
+        enqueue(object : Callback<T> {
+            override fun onResponse(call: Call<T>?, response: Response<T>) {
+                continuation.resumeWith(runCatching {
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        if (body == null) {
+                            ApiResponse(NullPointerException("Response body is null"))
+                        } else {
+                            ApiResponse(response)
+                        }
+                    } else {
+                        ApiResponse(HttpException(response))
+                    }
+                })
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                // Don't bother with resuming the continuation if it is already cancelled.
+                if (continuation.isCancelled) return
+                continuation.resume(ApiResponse(t))
+            }
+        })
+
+        registerOnCompletion(continuation)
+    }
+}
+
+private fun Call<*>.registerOnCompletion(continuation: CancellableContinuation<*>) {
+    continuation.invokeOnCancellation {
+        try {
+            cancel()
+        } catch (ex: Throwable) {
+            //Ignore cancel exception
+        }
+    }
+}
