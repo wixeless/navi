e95a83c859e2ba682a2012a6e4b0e446c0ab2c89
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-11-29 21:25:09 +0600

    hide\show bottomNavigation on destination change

--- app/src/main/java/com/marvel/stark/ui/main/MainActivity.kt
+++ app/src/main/java/com/marvel/stark/ui/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.marvel.stark.ui.main
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.addListener
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -34,6 +37,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope, HasAndroidInjector {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    private val bottomNavFrags = setOf(R.id.home_dest, R.id.worker_dest, R.id.payout_dest)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,12 +54,48 @@ class MainActivity : AppCompatActivity(), CoroutineScope, HasAndroidInjector {
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigation.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            changeBottomNavigationState(destination.id in bottomNavFrags)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         // Allows NavigationUI to support proper up navigation or the drawer layout
         // drawer menu, depending on the situation
         return findNavController(R.id.host_fragment).navigateUp(appBarConfiguration)
+    }
+
+    private fun changeBottomNavigationState(show: Boolean) {
+
+        if (show) {
+            if (bottomNavigation.visibility == View.VISIBLE)
+                return
+        } else {
+            if (bottomNavigation.visibility == View.GONE)
+                return
+        }
+        val height = bottomNavigation.height.toFloat()
+        val animator: ObjectAnimator
+        animator = if (show) {
+            ObjectAnimator.ofFloat(bottomNavigation, "translationY", height, 0f)
+        } else {
+            ObjectAnimator.ofFloat(bottomNavigation, "translationY", 0f, height)
+        }
+
+        animator.duration = 500
+        animator.addListener(
+                onStart = {
+                    if (show) {
+                        bottomNavigation.visibility = View.VISIBLE
+                    }
+                },
+                onEnd = {
+                    if (!show) {
+                        bottomNavigation.visibility = View.GONE
+                    }
+                }
+        )
+        animator.start()
     }
 
     override fun androidInjector() = dispatchingAndroidInjector
