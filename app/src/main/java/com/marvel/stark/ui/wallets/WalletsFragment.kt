a917ca48b0d9732e115816ca1a0f445f95eaecac
Jahongir <jahongir.bekmuhammetov@gmail.com>
2019-11-29 21:24:32 +0600

    hide\show fab on navigation

--- app/src/main/java/com/marvel/stark/ui/wallets/WalletsFragment.kt
+++ app/src/main/java/com/marvel/stark/ui/wallets/WalletsFragment.kt
@@ -1,6 +1,7 @@
 package com.marvel.stark.ui.wallets
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +10,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.marvel.stark.R
 import com.marvel.stark.adapter.WalletsAdapter
 import com.marvel.stark.di.factory.Injectable
 import com.marvel.stark.shared.di.ViewModelFactory
-import com.marvel.stark.shared.result.Status.*
+import com.marvel.stark.shared.result.Status.ERROR
+import com.marvel.stark.shared.result.Status.LOADING
 import com.marvel.stark.utils.toastMessage
 import kotlinx.android.synthetic.main.fragment_wallets.*
 import javax.inject.Inject
@@ -24,7 +27,6 @@ class WalletsFragment : Fragment(), Injectable {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
-
 
 
     private val walletsViewModel: WalletsViewModel by viewModels { viewModelFactory }
@@ -64,12 +66,26 @@ class WalletsFragment : Fragment(), Injectable {
         }
     }
 
+    override fun onResume() {
+        Handler().postDelayed({
+            try {
+                fab.show()
+            } catch (skip: NullPointerException) {
+            }
+        }, 400)
+        super.onResume()
+    }
+
     private fun setupWalletsRv() {
         wallets_rv.layoutManager = LinearLayoutManager(context)
         wallets_rv.adapter = walletsAdapter
         walletsAdapter.setWalletClickListener { wallet ->
-            val action = WalletsFragmentDirections.actionOpenWallet(wallet.id)
-            findNavController().navigate(action)
+            fab.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
+                override fun onHidden(fab: FloatingActionButton?) {
+                    val action = WalletsFragmentDirections.actionOpenWallet(wallet.id)
+                    findNavController().navigate(action)
+                }
+            })
         }
     }
 }
